2ef31e2e7ecc6a5b79afc1e2e31d66dca72bb7b3
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-08-19 18:30:41 +0800

    updated teh homeview to be stateful will make each view the state controller and components all functional

--- src/features/HomeView.js
+++ src/features/HomeView.js
@@ -1,28 +1,69 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ShowItem from './components/ShowItem';
 import ApiHandler from '../services/http/ApiHandler';
-const HomeView = () => {
-    let showList = [];
+
+
+class HomeView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showList: [],
+            test: 'THis is a test'
+        };
+    }
+
+    render() {
+
+        let showList = [];
 
     const apiHandler = new ApiHandler();
     apiHandler.getNowAiring()
         .then(response => response.response.results)
         .then(shows => {
             for (let i = 0; i < shows.length; i++) {
-                showList.push(<ShowItem show={shows[i]} />);
+                showList.push(<ShowItem show={shows[i]} key={shows[i].id} />);
             }
+            this.setState({
+                showList: showList
+            });
 
         });
 
-    return (
+        return (
         <div className='home-container'>
             <h1>Default homepage</h1>
             <div className='showGrid'>
-                {showList}
+                {this.state.showList}
             </div>
         </div>
-    );
+        );
+    }
+
 }
+
+// const HomeView = () => {
+//     let showList = [];
+
+//     const apiHandler = new ApiHandler();
+//     apiHandler.getNowAiring()
+//         .then(response => response.response.results)
+//         .then(shows => {
+//             for (let i = 0; i < shows.length; i++) {
+//                 showList.push(<ShowItem show={shows[i]} />);
+//             }
+
+//         });
+
+//     return (
+//         <div className='home-container'>
+//             <h1>Default homepage</h1>
+//             <div className='showGrid'>
+//                 {showList}
+//             </div>
+//         </div>
+//     );
+// }
 
 
 export default HomeView;
--- src/features/components/ShowItem.js
+++ src/features/components/ShowItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 const ShowItem = (props) => {
-    console.log(props.show);
     return(
         <div>
             <p>Main item object</p>
