4285b32c3f62f5f8794569d40ee9397ea63b05cc
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-08-26 20:00:06 +0800

    working on the overall style

--- src/features/components/DetailComponents/ShowsSideInfo.js
+++ src/features/components/DetailComponents/ShowsSideInfo.js
@@ -3,9 +3,19 @@ import GlobalVar from '../../../data/GlobalVars';
 
 const ShowSideInfo = (props) => {
 
+    const imgUri = GlobalVar.apiPosterUri + props.show.poster_path;
+
     return (
         <div className='detail-left-container'>
-            Side info
+            <div className="show-image">
+                <img src={imgUri} alt=""/>
+            </div>
+            <h3 className="my-subheader show-title">
+                {props.show.name}
+            </h3>
+            <p>
+              {props.show.overview}  
+            </p>
         </div>
     );
 }
