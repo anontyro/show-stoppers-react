2c58f40e304dd02dc1d0b969541d7966674ffc2e
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-09-26 17:20:13 +0800

    updated the show image item to return a placeholder image when there is not image path for that specific item

--- src/features/components/ShowItem.js
+++ src/features/components/ShowItem.js
@@ -6,11 +6,29 @@ import './ShowItem.css';
 const ShowItem = (props) => {
   const imgUri = GlobalVars.apiPosterUri + props.show.poster_path;
   const showLink = `/show/${props.show.id}`;
+
+  const renderImg = () => {
+    let renderImg;
+
+    if (props.show.poster_path === null) {
+      renderImg = (
+        <img className='poster' src="https://via.placeholder.com/150x255" alt="placeholder image"/>
+      );
+    } else {
+        renderImg = (
+          <img className='poster' src={imgUri} alt=""/>
+        );
+    }
+
+    return renderImg;
+  }
+
   return (
+
     <Link to={showLink}>
       <div className='show-container'>
         <div className="img-container">
-          <img className='poster' src={imgUri} alt=""/>
+          {renderImg()}
         </div>
         <p className='show-title'>{props.show.name}</p>
           </div>       
